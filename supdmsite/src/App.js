dc2d0636ce11e77e088be1ff08ec4af06a524eca
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-07-19 00:20:55 -0400

    -
    -

--- supdmsite/src/App.js
+++ supdmsite/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 
 class App extends React.Component {
   render (){
+    function Pro() {
+      alert("Coming Soon!");
+    }
     return (
       <div className="App">
             <div className="psa lykdat"><b>Sponsored:&nbsp;</b><span>Shop for fashion by searching with images on &nbsp;</span><b>LykDat &nbsp;</b><a className="btn btn-primary" href="https://lykdat.com/?utm_source=igdm&amp;utm_medium=banner" target="_blank" rel="noopener noreferrer"><b>TRY IT NOW</b></a></div>
@@ -15,6 +18,7 @@ class App extends React.Component {
                   <li className="nav-item"><a className="nav-link" href="/changelog">Changelog</a></li>
                   <li className="nav-item"><a className="nav-link" href="/blog">Blog</a></li>
                   <li className="nav-item"><a className="nav-link" href="/download">Download</a></li>
+                  <li className="nav-item"><a className="nav-link" onClick={Pro} id="bot">Instagram Bot</a></li>
           </ul>
         </div>
       </nav>
@@ -67,20 +71,20 @@ class App extends React.Component {
           </div>
           <div className="col-md-4"></div>
         </div>
-        <div className="screenshots row mt-5 text-center"><a className="col-md-4 mb-4" href="img/startchat.gif" alt="SupremeDM" target="_blank">Search for users and start a conversation<img className="mt-3 img-fluid" src="img/startchat.gif" alt="SupremeDM"/></a><a className="col-md-4 mb-4" href="img/unfollowers.gif" target="_blank">View a list of users not following you back<img className="mt-3 img-fluid" src="img/unfollowers.gif" alt="SupremeDM" /></a><a className="col-md-4 mb-4" href="img/quotemessage.gif" alt="SupremeDM" target="_blank">Quote a chat message<img className="mt-3 img-fluid" src="img/quotemessage.gif" alt="SupremeDM" /></a><a className="col-md-4" href="img/viewimage.gif" target="_blank">View chat images<img className="mt-3 img-fluid" src="img/viewimage.gif" alt="SupremeDM"/></a></div>
+        <div className="screenshots row mt-5 text-center"><a className="col-md-4 mb-4" href="img/login.gif" alt="SupremeDM" target="_blank">Login to Instagram<img className="mt-3 img-fluid" src="img/login.gif" alt="SupremeDM" /></a><a className="col-md-4 mb-4" href="img/startchat.gif" alt="SupremeDM" target="_blank">Search for users and start a conversation<img className="mt-3 img-fluid" src="img/startchat.gif" alt="SupremeDM"/></a><a className="col-md-4 mb-4" href="img/unfollowers.gif" target="_blank">View a list of users not following you back<img className="mt-3 img-fluid" src="img/unfollowers.gif" alt="SupremeDM" /></a><a className="col-md-4" href="img/viewimage.gif" target="_blank">View chat images<img className="mt-3 img-fluid" src="img/viewimage.gif" alt="SupremeDM"/></a></div>
       </section>
       <section className="issues mt-5 text-center">
         <h2>Want more features?</h2>
-        <h4><a className="btn btn-primary prominent2" href="/upgrade" target="_blank">Try &nbsp;<b>SupremeDM Pro</b></a></h4>
+        <h4><button className="btn btn-primary prominent2" onClick={Pro}>Try &nbsp;<b>SupremeDM Pro</b></button></h4>
       </section>
       <section className="row mt-5 justify-content-center">
         <div className="col-md-5">
-          <h4>Issues running SupremeDM on your computer?</h4><span>Please see <a href="/blog/igdm-on-windows-linux" target="_blank">common issues and fixes </a>for running SupremeDM on your desktop.</span>
+          <h4>Issues running SupremeDM on your computer?</h4><span>Please see <a href="/blog" >common issues and fixes </a>for running SupremeDM on your desktop.</span>
         </div>
         <div className="col-md-1"></div>
         <div className="col-md-5">
           <h4>Donations</h4>SupremeDM
-          was an open source project and the original source can be used for free by anyone, donations
+          was developed from an open source project and the original source can be used for free by anyone, donations
           and incentives are wholeheartedly welcome.<a className="btn btn-link btn-outline-light" role="button" href="http://paypal.me/supremeciento" target="_blank" rel="noopener noreferrer">Donate via PayPal</a>
         </div>
       </section>
@@ -89,7 +93,7 @@ class App extends React.Component {
       <div className="footerBackground"></div>
       <div className="footerWrapper container text-center">
         <p className="col-md-8 mx-auto">Please note that this is project ais not affiliated with Instagram.</p>
-        <p className="mb-0">Copyright © 2020 Cloud Bots™ SupremeDM.</p>
+        <p className="mb-0">Copyright © 2020 Cloud Bots™ SupremeDM</p>
       </div>
     </footer>
       </div>
